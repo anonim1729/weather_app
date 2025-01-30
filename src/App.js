4252214277d75872c92ac9ea3d6256cfad42b0b3
Naren <coding.demon.naren@gmail.com>
2025-01-30 17:24:41 +0530

    changed from http -> https for secure api calls

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { useState, useEffect } from 'react';
 import Input from './Input';
 import Content from './Content';
-const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 function App() {
-  const GEO_URL = 'http://api.openweathermap.org/geo/1.0/direct?';
-  const API_KEY = apiKey;
-  const WET_URL = 'https://api.openweathermap.org/data/2.5/weather?'
+  const GEO_URL = 'https://api.openweathermap.org/geo/1.0/direct?';
+  const WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?';
+
   const [place, setPlace] = useState('bengaluru');
   const [input, setInput] = useState('');
   const [currentWeather, setCurrentWeather] = useState({});
@@ -14,36 +15,38 @@ function App() {
   useEffect(() => {
     const fetchWeather = async (lat, lon) => {
       try {
-        const response = await fetch(`${WET_URL}lat=${lat}&lon=${lon}&appid=${API_KEY}`);
-        if (!response.ok) throw Error("Something went wrong");
+        const response = await fetch(`${WEATHER_URL}lat=${lat}&lon=${lon}&appid=${API_KEY}`);
+        if (!response.ok) throw new Error("Something went wrong");
         const weather = await response.json();
         console.log(weather);
         setCurrentWeather(weather);
       } catch (err) {
         console.log(err);
       }
-    }
+    };
 
     const fetchLonLat = async () => {
       try {
         const response = await fetch(`${GEO_URL}q=${place}&limit=1&appid=${API_KEY}`);
-        if (!response.ok) throw Error("Something went wrong");
+        if (!response.ok) throw new Error("Something went wrong");
         const data = await response.json();
         console.log(data);
-        await fetchWeather(data[0].lat, data[0].lon);
+        if (data.length > 0) {
+          await fetchWeather(data[0].lat, data[0].lon);
+        }
       } catch (err) {
         console.log(err);
       }
-    }
+    };
 
-    (async () => { await fetchLonLat() })();
-  }, [place, API_KEY])
+    fetchLonLat();
+  }, [place]);
 
   const handleOnClick = () => {
     setInput('');
     setPlace(input);
     console.log(currentWeather);
-  }
+  };
 
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-cover bg-fixed font-sans" style={{ backgroundImage: 'url("/background.jpg")' }}>
